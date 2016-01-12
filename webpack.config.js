1dda1603e1efb694901cec3e0e8f81a93cb72a20
Ben Goertz <bengoertz@gmail.com>
2016-01-12 12:02:57 -0800

    Oh how I love webpack...

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+var os = require("os");
+var host = os.hostname();
+var buildPath = path.resolve(__dirname, 'dist');
+
 module.exports = {
-  context: __dirname + "/app",
-  entry: {
-	javascript: "./app.js",
-	html: "./index.html",
-  },
-  output: {
-	filename: "app.js",
-	path: __dirname + "/dist",
-  },
-  module: {
-	loaders: [
-	  {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		loaders: ["babel-loader"],
-	  },
-	  {
-		test: /\.html$/,
-		loader: "file?name=[name].[ext]",
-	  },
-	],
-  },
-}
+  	entry: './src/app.jsx',
+	output: {
+		filename: 'bundle.js',
+		path: buildPath,
+	},
+	module: {
+		loaders: [
+			{
+			  loader: "babel-loader",
+
+			  // Skip any files outside of your project's `src` directory
+			  include: [
+				path.resolve(__dirname, "src"),
+			  ],
+
+			  // Only run `.js` and `.jsx` files through Babel
+			  test: /\.jsx?$/,
+
+			  // Options to configure babel with
+			  query: {
+				presets: ['es2015', 'react'],
+			  }
+			},
+		]
+	}
+};
