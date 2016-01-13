25b1904418c77dc46af3bf255faf96e2a9e0d711
Ben Goertz <bengoertz@gmail.com>
2016-01-13 14:11:05 -0800

    Adding input for creating new tasks.

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -2,4 +2,16 @@ import React from 'react'
 
 export default class AddTodo extends React.Component {
 	
+	render() {
+		return (
+			<div>
+				<form onSubmit = { (event) => this.handleSubmit(event) }>
+					<input type="text" ref="input" />
+					<button>
+						Add
+					</button>
+				</form>
+			</div>
+		)
+	}
 }
