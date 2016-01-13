4f4957b3e824f51c9f899c0ccb144d2671c65c8a
Ben Goertz <bengoertz@gmail.com>
2016-01-13 14:57:22 -0800

    Passing down main props to AddTodo and TodoList.

--- components/App.js
+++ components/App.js
@@ -3,3 +3,20 @@ import { connect } from 'react-redux'
 import { addTodo, completeTodo } from '../actions'
 import AddTodo from './AddTodo'
 import TodoList from './TodoList'
+
+class App extends React.Component {
+	render() {
+		const { dispatch, visibleTodos } = this.props
+		return (
+			<div>
+				<AddTodo
+					onAddSubmit = { text => dispatch(addTodo(text))} 
+				/>
+				<TodoList
+					todos = { visibleTodos }
+					onTodoClick = { id => dispatch(completeTodo(id))}
+				/>
+			</div>
+		)
+	}
+}
