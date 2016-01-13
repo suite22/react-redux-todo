a8cbef5aff28197d446a2c7f5543b83c630fb35e
Ben Goertz <bengoertz@gmail.com>
2016-01-13 14:54:24 -0800

    Handle adding a new todo item. --- preventDefault() is really interesting - https://jsfiddle.net/api/mdn/

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export default class AddTodo extends React.Component {
+	handleSubmit(event) {
+		event.preventDefault()
+		const node = this.refs.input
+		const text = node.value.trim()
+		if (text) {
+			this.props.onAddSubmit(text)
+			node.value = ''
+		}
+	}
 	
 	render() {
 		return (
