918a29a333893690bbbd4fd2d3c84cdc5e334af1
Ben Goertz <bengoertz@gmail.com>
2016-01-13 15:29:31 -0800

    Strike through completed tasks.

--- components/Todo.js
+++ components/Todo.js
@@ -5,6 +5,10 @@ export default class Todo extends React.Component {
 		return (
 			<li
 				onClick = { this.props.onClick }
+				style={{
+        			textDecoration: this.props.completed ? 'line-through' : 'none',
+        			cursor: this.props.completed ? 'default' : 'pointer'
+        		}}
 			>
 			{ this.props.text }
 			</li>
