d4fe7eb70d1dca3c21551b5e90ff8dddc8d33997
Ben Goertz <bengoertz@gmail.com>
2016-01-14 10:48:24 -0800

    Using a checkbox to toggle todos so that clicking the text can edit the text instead.

--- components/Todo.js
+++ components/Todo.js
@@ -3,15 +3,20 @@ import React from 'react'
 export default class Todo extends React.Component {
 	render() {
 		return (
-			<li
-				onClick = { this.props.onClick }
-				style={{
-        			textDecoration: this.props.completed ? 'line-through' : 'none',
-        			cursor: this.props.completed ? 'default' : 'pointer'
-        		}}
-			>
-			{ this.props.text }
-			</li>
+			<div>
+				<input 
+					type = "checkbox"
+					onClick = { this.props.onClick }
+				/>
+				<li
+					style={{
+	        			textDecoration: this.props.completed ? 'line-through' : 'none',
+	        			cursor: this.props.completed ? 'default' : 'pointer'
+	        		}}
+				>
+				{ this.props.text }
+				</li>
+			</div>
 		)
 	}
 }
